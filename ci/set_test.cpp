39c965213dfe1f97235a223f51bacd55e0c3cc0c
Łukasz Krzyściak <lkrzysciak@gmail.com>
2020-10-09 15:02:40 +0200

    Improved set insert test

--- ci/set_test.cpp
+++ ci/set_test.cpp
@@ -119,14 +119,16 @@ TYPED_TEST(SetTest, Insert)
     int temp1{ 3215 };
     int temp2{ 23587 };
     int temp3{ 980 };
+    int temp4{ 1024 };
+    int temp5{ 5005 };
 
-    std::set<int> expected_set{temp1, temp2, temp3};
+    std::set<int> expected_set{temp1, temp2, temp3, temp4, temp5};
 
     ASSERT_EQ(Insert(&container, temp1), 1);
     ASSERT_EQ(Insert(&container, temp2), 2);
     ASSERT_EQ(Insert(&container, temp3), 3);
-
-    printf("%d %d %d\n", container.root->left->value, container.root->value, container.root->right->value);
+    ASSERT_EQ(Insert(&container, temp4), 4);
+    ASSERT_EQ(Insert(&container, temp5), 5);
 
     std::set<int> to_compare_set{};
 
@@ -139,6 +141,6 @@ TYPED_TEST(SetTest, Insert)
         IteratorInc(&it);
     }    
     ASSERT_EQ(expected_set, to_compare_set);
-    ASSERT_EQ(Size(&container), 3);
+    ASSERT_EQ(Size(&container), 5);
     ASSERT_FALSE(Empty(&container));
 }
