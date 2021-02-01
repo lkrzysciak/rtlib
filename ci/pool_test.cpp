4080add1f4b80303379f00ee245133625493ab5b
Łukasz Krzyściak <lkrzysciak@gmail.com>
2021-02-01 17:24:00 +0000

    Reformated new files with clang

--- ci/pool_test.cpp
+++ ci/pool_test.cpp
@@ -1,36 +1,28 @@
 #include "gtest/gtest.h"
+
+extern "C"
+{
 #include "memory/dynamic_allocator.h"
 #include "memory/custom_allocator_pool.h"
 #include "memory/static_pool.h"
 
-declare_static_pool_t(StaticPoolInt, int, 5);
-define_static_pool_t(StaticPoolInt, int, 5);
+    declare_static_pool_t(StaticPoolInt, int, 5);
+    define_static_pool_t(StaticPoolInt, int, 5);
 
-declare_dynamic_allocator_t(DynamicAllocator);
-define_dynamic_allocator_t(DynamicAllocator);
-declare_custom_allocator_pool_t(CustomAllocatorPoolInt, int, DynamicAllocator);
-define_custom_allocator_pool_t(CustomAllocatorPoolInt, int, DynamicAllocator);
-
-#define create_wrappers_for_type(Type, MemberType) \
-void Init(Type* const container) \
-{ \
-    Type##_Construct(container); \
-} \
-\
-void Deinit(Type* const container) \
-{ \
-    Type##_Destroy(container); \
-} \
-\
-MemberType* Allocate(Type* const container) \
-{ \
-    return Type##_Allocate(container); \
-} \
-\
-void Release(Type* const container, MemberType* object) \
-{ \
-    Type##_Release(container, object); \
+    declare_dynamic_allocator_t(PoolDynamicAllocator);
+    define_dynamic_allocator_t(PoolDynamicAllocator);
+    declare_custom_allocator_pool_t(CustomAllocatorPoolInt, int, PoolDynamicAllocator);
+    define_custom_allocator_pool_t(CustomAllocatorPoolInt, int, PoolDynamicAllocator);
 }
+
+#define create_wrappers_for_type(Type, MemberType)                                       \
+    void Init(Type * const container) { Type##_Construct(container); }                   \
+                                                                                         \
+    void Deinit(Type * const container) { Type##_Destroy(container); }                   \
+                                                                                         \
+    MemberType * Allocate(Type * const container) { return Type##_Allocate(container); } \
+                                                                                         \
+    void Release(Type * const container, MemberType * object) { Type##_Release(container, object); }
 
 create_wrappers_for_type(StaticPoolInt, int);
 create_wrappers_for_type(CustomAllocatorPoolInt, int);
@@ -38,33 +30,24 @@ create_wrappers_for_type(CustomAllocatorPoolInt, int);
 template<typename T>
 struct PoolTest : public testing::Test
 {
-    void SetUp() override 
-    {
-        Init(&pool);
-    }
+    void SetUp() override { Init(&pool); }
 
-    void TearDown() override 
-    {
-        Deinit(&pool);
-    }
+    void TearDown() override { Deinit(&pool); }
 
     T pool;
 };
 
-using MyTypes = testing::Types<
-    StaticPoolInt,
-    CustomAllocatorPoolInt
-        >;
+using MyTypes = testing::Types<StaticPoolInt, CustomAllocatorPoolInt>;
 TYPED_TEST_SUITE(PoolTest, MyTypes);
 
 TYPED_TEST(PoolTest, Alloc)
 {
-    int* var1{Allocate(&this->pool)};
-    int* var2{Allocate(&this->pool)};
-    int* var3{Allocate(&this->pool)};
-    int* var4{Allocate(&this->pool)};
-    int* var5{Allocate(&this->pool)};
-    
+    int * var1{ Allocate(&this->pool) };
+    int * var2{ Allocate(&this->pool) };
+    int * var3{ Allocate(&this->pool) };
+    int * var4{ Allocate(&this->pool) };
+    int * var5{ Allocate(&this->pool) };
+
     *var1 = 1;
     *var2 = 2;
     *var3 = 3;
@@ -75,16 +58,16 @@ TYPED_TEST(PoolTest, Alloc)
     ASSERT_EQ(*var2, 2);
     ASSERT_EQ(*var3, 3);
     ASSERT_EQ(*var4, 4);
-    ASSERT_EQ(*var5, 5);    
+    ASSERT_EQ(*var5, 5);
 }
 
 TYPED_TEST(PoolTest, AllocAndRelease)
 {
-    int* var1{Allocate(&this->pool)};
-    int* var2{Allocate(&this->pool)};
-    int* var3{Allocate(&this->pool)};
-    int* var4{Allocate(&this->pool)};
-    int* var5{Allocate(&this->pool)};
+    int * var1{ Allocate(&this->pool) };
+    int * var2{ Allocate(&this->pool) };
+    int * var3{ Allocate(&this->pool) };
+    int * var4{ Allocate(&this->pool) };
+    int * var5{ Allocate(&this->pool) };
 
     Release(&this->pool, var1);
     Release(&this->pool, var2);
@@ -98,23 +81,23 @@ TEST(StaticPoolTest, AllocOverBufferSize)
     StaticPoolInt pool{};
     Init(&pool);
 
-    int* var1{Allocate(&pool)};
-    int* var2{Allocate(&pool)};
-    int* var3{Allocate(&pool)};
-    int* var4{Allocate(&pool)};
-    int* var5{Allocate(&pool)};
-    int* var6{Allocate(&pool)};
-    int* var7{Allocate(&pool)};
+    int * var1{ Allocate(&pool) };
+    int * var2{ Allocate(&pool) };
+    int * var3{ Allocate(&pool) };
+    int * var4{ Allocate(&pool) };
+    int * var5{ Allocate(&pool) };
+    int * var6{ Allocate(&pool) };
+    int * var7{ Allocate(&pool) };
 
-     ASSERT_NE(var1, nullptr);
-     ASSERT_NE(var2, nullptr);
-     ASSERT_NE(var3, nullptr);
-     ASSERT_NE(var4, nullptr);
-     ASSERT_NE(var5, nullptr);
-     ASSERT_EQ(var6, nullptr);
-     ASSERT_EQ(var7, nullptr);
+    ASSERT_NE(var1, nullptr);
+    ASSERT_NE(var2, nullptr);
+    ASSERT_NE(var3, nullptr);
+    ASSERT_NE(var4, nullptr);
+    ASSERT_NE(var5, nullptr);
+    ASSERT_EQ(var6, nullptr);
+    ASSERT_EQ(var7, nullptr);
 
-     Deinit(&pool);
+    Deinit(&pool);
 }
 
 TEST(StaticPoolTest, AllocFreeAtBufferEnd)
@@ -122,12 +105,12 @@ TEST(StaticPoolTest, AllocFreeAtBufferEnd)
     StaticPoolInt pool{};
     Init(&pool);
 
-    int* var1{Allocate(&pool)};
-    int* var2{Allocate(&pool)};
-    int* var3{Allocate(&pool)};
-    int* var4{Allocate(&pool)};
-    int* var5{Allocate(&pool)};
-    
+    int * var1{ Allocate(&pool) };
+    int * var2{ Allocate(&pool) };
+    int * var3{ Allocate(&pool) };
+    int * var4{ Allocate(&pool) };
+    int * var5{ Allocate(&pool) };
+
     *var1 = 1;
     *var2 = 2;
     *var3 = 3;
@@ -135,8 +118,8 @@ TEST(StaticPoolTest, AllocFreeAtBufferEnd)
     *var5 = 5;
 
     Release(&pool, var3);
-    int* var6{Allocate(&pool)};
-    int* var7{Allocate(&pool)};
+    int * var6{ Allocate(&pool) };
+    int * var7{ Allocate(&pool) };
 
     *var6 = 6;
 
@@ -144,8 +127,8 @@ TEST(StaticPoolTest, AllocFreeAtBufferEnd)
     ASSERT_EQ(*var2, 2);
     ASSERT_EQ(*var4, 4);
     ASSERT_EQ(*var5, 5);
-    ASSERT_EQ(*var6, 6);    
-    ASSERT_EQ(var7, nullptr);    
+    ASSERT_EQ(*var6, 6);
+    ASSERT_EQ(var7, nullptr);
 
     Deinit(&pool);
 }
@@ -155,16 +138,16 @@ TEST(StaticPoolTest, AllocFreeAtBufferBegin)
     StaticPoolInt pool{};
     Init(&pool);
 
-    int* var1{Allocate(&pool)};
+    int * var1{ Allocate(&pool) };
     Release(&pool, var1);
 
-    int* var2{Allocate(&pool)};
-    int* var3{Allocate(&pool)};
-    int* var4{Allocate(&pool)};
-    int* var5{Allocate(&pool)};
-    int* var6{Allocate(&pool)};
-    int* var7{Allocate(&pool)};
-    
+    int * var2{ Allocate(&pool) };
+    int * var3{ Allocate(&pool) };
+    int * var4{ Allocate(&pool) };
+    int * var5{ Allocate(&pool) };
+    int * var6{ Allocate(&pool) };
+    int * var7{ Allocate(&pool) };
+
     *var2 = 2;
     *var3 = 3;
     *var4 = 4;
@@ -175,8 +158,8 @@ TEST(StaticPoolTest, AllocFreeAtBufferBegin)
     ASSERT_EQ(*var3, 3);
     ASSERT_EQ(*var4, 4);
     ASSERT_EQ(*var5, 5);
-    ASSERT_EQ(*var6, 6);    
-    ASSERT_EQ(var7, nullptr);    
+    ASSERT_EQ(*var6, 6);
+    ASSERT_EQ(var7, nullptr);
 
     Deinit(&pool);
 }
